1b15e3cefff1ff4d1c0acf2d8cda09b0d4a6842e
Matt Blewitt <blewitt.matt@gmail.com>
2016-06-12 23:56:06 +0100

    remove ParallelQuicksort

--- lib/sorting_algorithms.rb
+++ lib/sorting_algorithms.rb
@@ -1,12 +1,10 @@
 require 'sorting_algorithms/quicksort'
 require 'sorting_algorithms/mergesort'
-require 'sorting_algorithms/parallel_quicksort'
 require 'sorting_algorithms/rustsort'
 ##
 # Activate Array patches
 class Array
   include SortingAlgorithms::Quicksort
   include SortingAlgorithms::Mergesort
-  include SortingAlgorithms::ParallelQuicksort
   include SortingAlgorithms::Rustsort
 end
--- spec/benchmarks/sorted_bench.rb
+++ spec/benchmarks/sorted_bench.rb
@@ -8,7 +8,6 @@ Benchmark.ips do |x|
   x.report('Array#sort') { data.sort }
   x.report('Array#mergesort') { data.mergesort }
   x.report('Array#quicksort') { data.quicksort }
-  x.report('Array#parallel_quicksort') { data.parallel_quicksort }
   x.report('Array#rustsort') { data.rustsort }
   x.compare!
 end
