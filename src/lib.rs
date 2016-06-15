91d8a4c1879facbcbda31ff3de5dfc409b860335
Matt Blewitt <blewitt.matt@gmail.com>
2016-06-15 21:26:01 +0100

    reorganise rust sorts

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate libc;
-use std::mem;
-use std::slice;
+mod quick_sort;
+use std::{mem, slice};
+use quick_sort::quick_sort;
 
 #[repr(C)]
 pub struct RubyArray {
@@ -19,13 +20,6 @@ impl RubyArray {
     }
 }
 
-type OrderFunc<T> = Fn(&T, &T) -> bool;
-
-#[inline(always)]
-fn is_less<T: Ord>(x: &T, y: &T) -> bool {
-    x < y
-}
-
 #[no_mangle]
 pub extern "C" fn rustmergesort(n: *const libc::int32_t, len: libc::size_t) -> RubyArray {
     let numbers = unsafe {
@@ -33,10 +27,10 @@ pub extern "C" fn rustmergesort(n: *const libc::int32_t, len: libc::size_t) -> R
         slice::from_raw_parts(n, len as usize)
     };
     let mut mutable_numbers = numbers.to_owned();
-    mutable_numbers.sort(); // This is an insertion sort for a small vec
-                            // but a merge sort for larger vecs
-                            // https://github.com/rust-lang/rust/blob/master/src/libcollections/slice.rs#L1085
-
+    // This is an insertion sort for a small vec
+    // but a merge sort for larger vecs
+    // https://github.com/rust-lang/rust/blob/master/src/libcollections/slice.rs#L1085
+    mutable_numbers.sort();
     RubyArray::from_vec(mutable_numbers)
 }
 
@@ -52,31 +46,6 @@ pub extern "C" fn rustsort(n: *const libc::int32_t, len: libc::size_t) -> RubyAr
     RubyArray::from_vec(mutable_numbers)
 }
 
-fn quick_sort<T>(v: &mut [T], f: &OrderFunc<T>) {
-    let len = v.len();
-    if len < 2 {
-        return;
-    }
-
-    let pivot_index = partition(v, f);
-    quick_sort(&mut v[0..pivot_index], f);
-    quick_sort(&mut v[pivot_index + 1..len], f);
-}
-
-fn partition<T>(v: &mut [T], f: &OrderFunc<T>) -> usize {
-    let len = v.len();
-    let pivot_index = len / 2;
-
-    v.swap(pivot_index, len - 1);
-
-    let mut store_index = 0;
-    for i in 0..len - 1 {
-        if f(&v[i], &v[len - 1]) {
-            v.swap(i, store_index);
-            store_index += 1;
-        }
-    }
-
-    v.swap(store_index, len - 1);
-    store_index
+fn is_less<T: Ord>(x: &T, y: &T) -> bool {
+    x < y
 }
---
+++ src/quick_sort.rs
@@ -0,0 +1,30 @@
+pub type OrderFunc<T> = Fn(&T, &T) -> bool;
+
+pub fn quick_sort<T>(v: &mut [T], f: &OrderFunc<T>) {
+    let len = v.len();
+    if len < 2 {
+        return;
+    }
+
+    let pivot_index = partition(v, f);
+    quick_sort(&mut v[0..pivot_index], f);
+    quick_sort(&mut v[pivot_index + 1..len], f);
+}
+
+fn partition<T>(v: &mut [T], f: &OrderFunc<T>) -> usize {
+    let len = v.len();
+    let pivot_index = len / 2;
+
+    v.swap(pivot_index, len - 1);
+
+    let mut store_index = 0;
+    for i in 0..len - 1 {
+        if f(&v[i], &v[len - 1]) {
+            v.swap(i, store_index);
+            store_index += 1;
+        }
+    }
+
+    v.swap(store_index, len - 1);
+    store_index
+}
